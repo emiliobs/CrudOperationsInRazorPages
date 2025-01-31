e8350696218e82f9606e5dfb171f79e4e68636af
Emilio Barrera <barrera_emilio@yahoo.es>
2025-01-31 18:15:41 +0000

    Add project files.

---
+++ CrudOperationsInRazorPages/DAL/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudOperationsInRazorPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInRazorPages.DAL
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
---
+++ CrudOperationsInRazorPages/Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudOperationsInRazorPages.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Book Title")]
+        public string BookTitle { get; set; }
+
+        [DisplayName("Book Description")]
+        public string BookDescription { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+    }
+}
